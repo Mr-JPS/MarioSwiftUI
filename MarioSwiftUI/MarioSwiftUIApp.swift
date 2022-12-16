23ba00a83f72a3798dcca1f573f3e83ea2e28303
MrBeaverhausen <110383768+MrBeaverhausen@users.noreply.github.com>
2022-12-16 13:26:19 +0100

    Initial Commit

---
+++ MarioSwiftUI/MarioSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioSwiftUIApp.swift
+//  MarioSwiftUI
+//
+//  Created by Jean-Paul Sola on 16.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MarioSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
